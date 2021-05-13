643cbb806adebd351c0d48d5173e9319e25d9010
Anshika Mittal <anshika_m@cs.iitr.ac.in>
2021-05-13 21:34:37 +0530

    added sign in functionality

--- Simplab_App/android/app/src/screens/Team/router.js
+++ Simplab_App/android/app/src/screens/Team/router.js
@@ -15,50 +15,6 @@ import Files from './files';
 import bckImage from './TeamAssets/bckImg.png';
 import back from './TeamAssets/back.png';
 
-/* export default function Router() {
-  const Tab = createMaterialTopTabNavigator();
-  return (
-    <Tab.Navigator
-      initialRouteName="Chats"
-      screenOptions={{headerShown: true}}
-      tabBarOptions={{
-        style: {
-          backgroundColor: 'black',
-        },
-        labelStyle: {
-          fontSize: 14,
-          margin: 0,
-          padding: 0,
-        },
-        activeTintColor: '#F37A27',
-        inactiveTintColor: '#FFFFFF',
-      }}>
-      <Tab.Screen
-        name="Chats"
-        component={Chats}
-        options={{
-          tabBarLabel: 'Chats',
-        }}
-      />
-      <Tab.Screen
-        name="Experiments"
-        component={Experiments}
-        options={{
-          tabBarLabel: 'Experiments',
-        }}
-      />
-      <Tab.Screen
-        name="Files"
-        component={Files}
-        options={{
-          tabBarLabel: 'Files',
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
- */
-
 function viewShow(i) {
   if (i === 'Experiments') {
     return <Experiments />;
